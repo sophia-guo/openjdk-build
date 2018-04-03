f62ff54c48e5bdc1391392194aef02c807517da0
Sophia Guo <sophiag@ca.ibm.com>
2018-04-03 17:40:33 -0400

    Testing Deploy story
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- pipelines/openjdk9_nightly_pipeline.groovy
+++ pipelines/openjdk9_nightly_pipeline.groovy
@@ -1,6 +1,7 @@
 println "building ${JDK_VERSION}"
 
-def buildPlatforms = ['Mac', 'Linux', 'zLinux', 'ppc64le', 'Windows', 'AIX', 'arm64']
+//def buildPlatforms = ['Mac', 'Linux', 'zLinux', 'ppc64le', 'Windows', 'AIX', 'arm64']
+def buildPlatforms = ['Mac', 'Linux']
 def buildMaps = [:]
 buildMaps['Mac'] = [test:true, ArchOSs:'x86-64_macos']
 buildMaps['Windows'] = [test:false, ArchOSs:'x86-64_windows']
@@ -9,8 +10,8 @@ buildMaps['zLinux'] = [test:true, ArchOSs:'s390x_linux']
 buildMaps['ppc64le'] = [test:true, ArchOSs:'ppc64le_linux']
 buildMaps['AIX'] = [test:false, ArchOSs:'ppc64_aix']
 buildMaps['arm64'] = [test:true, ArchOSs:'aarch64_linux']
-def typeTests = ['openjdktest', 'systemtest']
-
+//def typeTests = ['openjdktest', 'systemtest']
+def typeTests = ['openjdktest']
 def jobs = [:]
 for ( int i = 0; i < buildPlatforms.size(); i++ ) {
 	def index = i
@@ -18,26 +19,36 @@ for ( int i = 0; i < buildPlatforms.size(); i++ ) {
 	def archOS = buildMaps[platform].ArchOSs
 	jobs[platform] = {
 		def buildJob
+		def buildJobNum
+		def checksumJob
 		stage('build') {
 			buildJob = build job: "openjdk9_build_${archOS}"
+			buildJobNum = buildJob.getNumber()
 		}
 		if (buildMaps[platform].test) {
 			stage('test') {
 				typeTests.each {
 					build job:"openjdk9_hs_${it}_${archOS}",
 							propagate: false,
-							parameters: [string(name: 'UPSTREAM_JOB_NUMBER', value: "${buildJob.getNumber()}"),
+							parameters: [string(name: 'UPSTREAM_JOB_NUMBER', value: "${buildJobNum}"),
 									string(name: 'UPSTREAM_JOB_NAME', value: "openjdk9_build_${archOS}")]
 				}
 			}
 		}
+		stage('checksums') {
+			checksumJob = build job: 'update_jdk9_checksum',
+							parameters: [string(name: 'SDKBUILDNAME', value: "openjdk9_build_${archOS}"),
+									string(name: 'BUILDNUMBER', value: "${buildJobNum}"),
+									string(name: 'PRODUCT', value: 'nightly')]
+		}
+		stage('publish nightly') {
+			build job: 'UpdateTool', 
+						parameters: [string(name: 'REPO', value: 'nightly'), 
+						string(name: 'TAG', value: 'jdk-9+181'), 
+						string(name: 'VERSION', value: 'jdk9'),
+						string(name: 'CHECKSUMBUILDNAME', value: "update_jdk9_checksum"),
+						string(name: 'BUILDNUMBER', value: "${checksumJob.getNumber()}")]
+		}
 	}
 }
 parallel jobs
-
-stage('checksums') {
-	build job: 'openjdk9_build_checksum'
-}
-stage('publish nightly') {
-	build job: 'openjdk_release_tool', parameters: [string(name: 'REPO', value: 'nightly'), string(name: 'TAG', value: 'jdk-9+181'), string(name: 'VERSION', value: 'jdk9')]
-}
--- pipelines/openjdk9_openj9_nightly_pipeline.groovy
+++ pipelines/openjdk9_openj9_nightly_pipeline.groovy
@@ -1,12 +1,14 @@
 println "building ${JDK_VERSION}"
 
-def buildPlatforms = ['Linux', 'zLinux', 'ppc64le', 'AIX']
+//def buildPlatforms = ['Linux', 'zLinux', 'ppc64le', 'AIX']
+def buildPlatforms = ['Linux', 'ppc64le']
 def buildMaps = [:]
 buildMaps['Linux'] = [test:true, ArchOSs:'x86-64_linux']
 buildMaps['zLinux'] = [test:true, ArchOSs:'s390x_linux']
 buildMaps['ppc64le'] = [test:true, ArchOSs:'ppc64le_linux']
 buildMaps['AIX'] = [test:false, ArchOSs:'ppc64_aix']
-def typeTests = ['openjdktest', 'systemtest']
+//def typeTests = ['openjdktest', 'systemtest']
+def typeTests = ['openjdktest']
 
 def jobs = [:]
 for ( int i = 0; i < buildPlatforms.size(); i++ ) {
@@ -15,26 +17,36 @@ for ( int i = 0; i < buildPlatforms.size(); i++ ) {
 	def archOS = buildMaps[platform].ArchOSs
 	jobs[platform] = {
 		def buildJob
+		def buildJobNum
+		def checksumJob
 		stage('build') {
 			buildJob = build job: "openjdk9_openj9_build_${archOS}"
+			buildJobNum = buildJob.getNumber()
 		}
 		if (buildMaps[platform].test) {
 			stage('test') {
 				typeTests.each {
 					build job:"openjdk9_j9_${it}_${archOS}",
 							propagate: false,
-							parameters: [string(name: 'UPSTREAM_JOB_NUMBER', value: "${buildJob.getNumber()}"),
+							parameters: [string(name: 'UPSTREAM_JOB_NUMBER', value: "${buildJobNum}"),
 									string(name: 'UPSTREAM_JOB_NAME', value: "openjdk9_openj9_build_${archOS}")]
 				}
 			}
+		}
+		stage('checksums') {
+			checksumJob = build job: 'update_jdk9_openj9_checksum',
+							parameters: [string(name: 'SDKBUILDNAME', value: "openjdk9_openj9_build_${archOS}"),
+									string(name: 'BUILDNUMBER', value: "${buildJobNum}")]
+		}
+		stage('publish nightly') {
+			build job: 'UpdateTool', 
+						parameters: [string(name: 'REPO', value: 'nightly'), 
+						string(name: 'TAG', value: 'jdk-9+181'), 
+						string(name: 'VERSION', value: 'jdk9-openj9'),
+						string(name: 'CHECKSUMBUILDNAME', value: "update_jdk9_openj9_checksum"),
+						string(name: 'BUILDNUMBER', value: "${checksumJob.getNumber()}")]
 		}
 	}
 }
 parallel jobs
 
-stage('checksums') {
-	build job: 'openjdk9_openj9_build_checksum'
-}
-stage('publish nightly') {
-	build job: 'openjdk_release_tool', parameters: [string(name: 'REPO', value: 'nightly'), string(name: 'TAG', value: 'jdk-9+181'), string(name: 'VERSION', value: 'jdk9-openj9')]
-}
